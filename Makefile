1028699fd82467211686d47615edf2f712d06fde
Maya Posch <1096451+MayaPosch@users.noreply.github.com>
2025-07-31 16:05:34 +0200

    Tweak names for example.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 
 all:
-	gfortran -o hello_science hello_science.f90
+	gfortran -o hello_world hello_science.f90
 	gfortran -o average average.f90
--- hello_science.f90
+++ hello_science.f90
@@ -1,3 +1,8 @@
-program helloworld
+! Hello_science.f90 - Simple MPI Coarray-based linear algebra processing example.
+! 
+! First revision - 2025/07/22 - Maya Posch
+
+program hello_world
+	implicit none
      print *, "Hello, World!"
-end program helloworld
+end program hello_world
